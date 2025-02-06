67000341893745854ccc31206d501df76486ab1c
avan31017 <avan31017@gmail.com>
2025-02-07 00:07:12 +0900

    [Feat] model, spacing 조절 input 추가 & 기능구현중

--- src/CanvasBox/CanvasBox.jsx
+++ src/CanvasBox/CanvasBox.jsx
@@ -2,47 +2,51 @@ import { useState } from "react"
 import { Canvas } from "@react-three/fiber"
 import Dummy3DModel from "./Dummy3DModel"
 import { OrbitControls } from "@react-three/drei"
+import { useEffect } from "react"
 
-function CanvasBox() {
-    const [numYModel, setNumYModel] = useState(4)
-    const [numXModel, setNumXModel] = useState(4)
-    const [modelSpacing, setModelSpacing] = useState(1.5)
-  
+function CanvasBox({ yModelCount, xModelCount, spacing }) {
+    const [numYModel, setNumYModel] = useState(yModelCount)
+    const [numXModel, setNumXModel] = useState(xModelCount)
+
     // 세로 부분 배치
     const createLeadingYModel = () => {
-      const yModels = []
-      for (let i = 1; i <= numYModel; i++) {
-        const z = (-i) * (modelSpacing)
-        yModels.push(
-          <Dummy3DModel key={`yModel - ${i}`} position={[0, 0, z]} color="blue" />
-        )
-      }
-      return yModels
+        const yModels = []
+        for (let i = 1; i <= numYModel; i++) {
+            const z = (-i) * (spacing)
+            yModels.push(
+                <Dummy3DModel key={`yModel - ${i}`} position={[0, 0, z]} color="blue" />
+            )
+        }
+        return yModels
     }
 
     const createTrailingYModel = () => {
         const yModels = []
         for (let i = 1; i <= numYModel; i++) {
-          // const x = (i % 2) * spacing
-          const z = (-i) * (modelSpacing)
-          yModels.push(
-            <Dummy3DModel key={`yModel - ${i}`} position={[(numXModel - 1) * (modelSpacing) , 0, z]} color="yellow" />
-          )
+            const z = (-i) * (spacing)
+            yModels.push(
+                <Dummy3DModel key={`yModel - ${i}`} position={[(numXModel - 1) * (spacing), 0, z]} color="yellow" />
+            )
         }
         return yModels
-      }
-  
+    }
+
     // 가로 부분 배치
     const createXModel = () => {
-      const xModels = []
-      for (let i = 0; i < numXModel; i++) {
-        const x = i * modelSpacing
-        xModels.push(
-          <Dummy3DModel key={`xModel - ${i}`} position={[x, 0, 0]} color="green" />
-        )
-      }
-      return xModels
+        const xModels = []
+        for (let i = 0; i < numXModel; i++) {
+            const x = i * spacing
+            xModels.push(
+                <Dummy3DModel key={`xModel - ${i}`} position={[x, 0, 0]} color="green" />
+            )
+        }
+        return xModels
     }
+
+    // useEffect(() => {
+    //     setNumYModel([...createLeadingYModel(), ...createTrailingYModel()])
+    //     setNumXModel(createXModel())
+    // }, [yModelCount, xModelCount, spacing])
 
     return (
         <Canvas>
