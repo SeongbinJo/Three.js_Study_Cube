59dab19d348521ba175b7fe815ebd4acc79c2bd0
avan31017 <avan31017@gmail.com>
2025-02-06 18:38:19 +0900

    [Feat] Dummy3DModel 자동회전

--- src/CanvasBox/Dummy3DModel.jsx
+++ src/CanvasBox/Dummy3DModel.jsx
@@ -1,8 +1,19 @@
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
 function Dummy3DModel() {
+    const refMesh = useRef()
+
+    // useFrame : 매 프레임이 렌더링되기 직전 호출
+    // delta : 이전, 다음 프레임 사이의 경과시간
+    useFrame((state, delta) => {
+        refMesh.current.rotation.y += 0.007
+    })
+
     return (
         <>
             <directionalLight position={[1,1,1]} />
-            <mesh rotation={[0, 45*Math.PI/180, 0]}>
+            <mesh ref={refMesh} rotation={[0, 0, 0]}>
                 <boxGeometry />
                 <meshStandardMaterial color="blue" />
             </mesh>
