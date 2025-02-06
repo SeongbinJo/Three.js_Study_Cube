32d8230ec9f3eb6616552c7a924f4f0c78126211
avan31017 <avan31017@gmail.com>
2025-02-06 18:26:48 +0900

    [Feat] Dummy3DModel 45도 회전

--- src/CanvasBox/Dummy3DModel.jsx
+++ src/CanvasBox/Dummy3DModel.jsx
@@ -2,7 +2,7 @@ function Dummy3DModel() {
     return (
         <>
             <directionalLight position={[1,1,1]} />
-            <mesh>
+            <mesh rotation={[0, 45*Math.PI/180, 0]}>
                 <boxGeometry />
                 <meshStandardMaterial color="blue" />
             </mesh>
