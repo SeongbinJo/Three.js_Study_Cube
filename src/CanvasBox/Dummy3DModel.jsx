85d2671f87fb73c0902dda55e506909fa499552e
avan31017 <avan31017@gmail.com>
2025-02-06 18:40:14 +0900

    [Fix] useRef, useFrame 주석처리

--- src/CanvasBox/Dummy3DModel.jsx
+++ src/CanvasBox/Dummy3DModel.jsx
@@ -2,18 +2,19 @@ import { useFrame } from "@react-three/fiber"
 import { useRef } from "react"
 
 function Dummy3DModel() {
-    const refMesh = useRef()
+    // const refMesh = useRef()
 
     // useFrame : 매 프레임이 렌더링되기 직전 호출
     // delta : 이전, 다음 프레임 사이의 경과시간
-    useFrame((state, delta) => {
-        refMesh.current.rotation.y += 0.007
-    })
+    // useFrame((state, delta) => {
+    //     refMesh.current.rotation.y += 0.007
+    // })
 
     return (
         <>
             <directionalLight position={[1,1,1]} />
-            <mesh ref={refMesh} rotation={[0, 0, 0]}>
+            
+            <mesh>
                 <boxGeometry />
                 <meshStandardMaterial color="blue" />
             </mesh>
