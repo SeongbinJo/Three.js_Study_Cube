e778bfcd883e3be1e711aa02989f5fba50eadc2b
avan31017 <avan31017@gmail.com>
2025-02-12 22:23:45 +0900

    [Feat] Model 클릭 이벤트 ing

--- src/Ver3/Ver3Model.jsx
+++ src/Ver3/Ver3Model.jsx
@@ -1,29 +1,44 @@
+import { useRef } from "react"
 import { useState } from "react"
 import * as THREE from "three"
 
 function Ver3Model({ id, position, color, onHover }) {
     const [hovered, setHovered] = useState(false)
 
+    const mouseOverHandler = (event) => {
+        event.stopPropagation()
+        setHovered(true)
+        onHover(true, event.clientX, event.clientY)
+    }
+
+    const mouseOutHandler = () => {
+        setHovered(false)
+        onHover(false)
+    }
+
+    const mouseClickHandler = () => {
+        console.log(id)
+    }
+
     return (
         <>
             <directionalLight position={[1, 1, 1]} />
 
-            <mesh position={position} onPointerOver={(event) => {
-                event.stopPropagation()
-                setHovered(true)
-                onHover(true, event.clientX, event.clientY, id)
-            }
-            }
-            onPointerMove={(event) => {
-                if (hovered) {
-                    onHover(true, event.clientX, event.clientY, id)
+            <mesh
+                position={position}
+                onPointerOver={
+                    mouseOverHandler
                 }
-            }}
-                onPointerOut={() => {
-                    setHovered(false)
-                    onHover(false)
+                onPointerMove={(event) => {
+                    if (hovered) {
+                        onHover(true, event.clientX, event.clientY, id)
+                    }
+                }}
+                onPointerOut={
+                    mouseOutHandler
                 }
-                }>
+                onClick={mouseClickHandler}
+            >
                 <boxGeometry args={[1, 1, 1]} />
                 <meshStandardMaterial color={color}
                     transparent={true}
