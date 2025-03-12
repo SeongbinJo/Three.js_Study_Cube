eaed1c506ecd6d296ad3590d7da60dd24831ae4e
avan31017 <avan31017@gmail.com>
2025-03-13 00:01:18 +0900

    SidePage2. code edit

--- src/SidePage2/SidePageModel2.jsx
+++ src/SidePage2/SidePageModel2.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react"
+import { useRef } from "react"
 import { TransformControls } from "@react-three/drei"
 import { useThree } from "@react-three/fiber"
 
@@ -18,11 +18,11 @@ function SidePageModel2({ orbitRef }) {
         onMouseDown={() => (orbitRef.current.enabled = false)}
         onMouseUp={() => (orbitRef.current.enabled = true)}
       >
-        <mesh position={[0, -0.75, 0]}>
+        <mesh position={[0, -1, 0]}>
           <sphereGeometry args={[0.5, 32, 16, 0, Math.PI * 2, 0, Math.PI / 2]} />
           <meshStandardMaterial color="blue" />
         </mesh>
-        <mesh position={[0, -1.75, 0]}>
+        <mesh position={[0, -1.95, 0]}>
         <cylinderGeometry args={[0.5, 0.5, 2]} />
         <meshStandardMaterial color="blue" />
       </mesh>
