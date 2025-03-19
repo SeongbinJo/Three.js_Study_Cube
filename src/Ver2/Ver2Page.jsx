34d8c77efe8375689ff001eb75ad3dea291e53ed
avan31017 <avan31017@gmail.com>
2025-03-19 17:58:04 +0900

    [Ver2] text 배치

--- src/Ver2/Ver2Page.jsx
+++ src/Ver2/Ver2Page.jsx
@@ -23,6 +23,16 @@ function Ver2Page() {
               {hoveredData.id}
             </div>
         )}
+        <div className='ver1-hovered-box' style={{ 
+                position: 'absolute',   // absolute로 지정하여 캔버스 위에 띄움
+                left: 30,               // 원하는 x 위치
+                top: 30,                // 원하는 y 위치
+                zIndex: 10             // 다른 요소들보다 위에 위치하도록 설정
+            }}>
+                [Ver2]<br />
+                1. 마우스 hover할 경우 해당 모델의 id 팝업<br />
+                2. 팝업된 id 창 마우스 따라다님
+            </div>
       </div>
     </>
   )
