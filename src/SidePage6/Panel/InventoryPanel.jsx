d25b9f38e3477e2bf8bcc4d74424a52926022cfc
avan31017 <avan31017@gmail.com>
2025-06-09 21:13:10 +0900

    [SidePage6] InventoryPanel UI edit..

--- src/SidePage6/Panel/InventoryPanel.jsx
+++ src/SidePage6/Panel/InventoryPanel.jsx
@@ -3,98 +3,115 @@ import { OrbitControls } from "@react-three/drei"
 import { SketchPicker } from "react-color"
 
 function InventoryPanel({
-  color,
-  setColor,
-  isGrid,
-  setIsGrid,
-  setBackgroundColor,
-  handleColorChange,
-  swatches,
-  selectedSwatchIndex,
-  setSelectedSwatchIndex,
-  setCreateBoxBtn,
-  setShowInventory
+    color,
+    setColor,
+    isGrid,
+    setIsGrid,
+    setBackgroundColor,
+    handleColorChange,
+    swatches,
+    selectedSwatchIndex,
+    setSelectedSwatchIndex,
+    setCreateBoxBtn,
+    setShowInventory
 }) {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top: "50%",
-        right: "50%",
-        transform: "translate(-50%, -50%)",
-        zIndex: 10,
-        backgroundColor: "green",
-        padding: "10px",
-        borderRadius: "5px",
-        maxWidth: "40%",
-      }}
-    >
-      <button
-        onClick={() => {
-          setCreateBoxBtn(true)
-          setShowInventory(prev => !prev)
-        }}
-      >
-        블럭 사용하기
-      </button>
-      <label style={{ display: 'inline-flex', alignItems: 'center', cursor: 'pointer', gap: '6px', marginLeft: '10px', marginRight: '10px' }}>
-        <input
-          type="checkbox"
-          checked={isGrid}
-          onChange={() => setIsGrid(prev => !prev)}
-        />
-        <span>Grid 적용</span>
-      </label>
-      <button onClick={() => setBackgroundColor(color)}>배경색 적용하기</button>
+    return (
+        <div
+            style={{
+                position: "fixed",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                zIndex: 10,
+                backgroundColor: "green",
+                padding: "10px",
+                borderRadius: "5px",
+                maxWidth: "80%",
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+                alignItems: "center"
+            }}
+        >
+            <div id="items" style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "10px",
+            }}>
+                <div id="canvas" style={{
+                    marginTop: "20px",
+                    backgroundColor: "lightgray",
+                    borderRadius: "5px",
+                    width: "100%"
+                }}>
+                    <Canvas camera={{ position: [1, 1, 5], fov: 30 }}>
+                        <OrbitControls autoRotate enablePan={false} enableZoom={false} />
+                        <directionalLight position={[10, 15, -30]} />
+                        <directionalLight position={[10, 30, -30]} />
+                        <directionalLight position={[20, -20, 30]} />
+                        <directionalLight position={[-10, 0, 0]} />
+                        <mesh>
+                            <boxGeometry args={[1, 1, 1]} />
+                            <meshStandardMaterial color={color} />
+                        </mesh>
+                    </Canvas>
+                </div>
 
-      <div style={{
-        marginTop: "20px",
-        backgroundColor: "lightgray",
-        borderRadius: "5px",
-        width: "100%"
-      }}>
-        <Canvas camera={{ position: [1, 1, 5], fov: 30 }}>
-          <OrbitControls autoRotate enablePan={false} enableZoom={false} />
-          <directionalLight position={[10, 15, -30]} />
-          <directionalLight position={[10, 30, -30]} />
-          <directionalLight position={[20, -20, 30]} />
-          <directionalLight position={[-10, 0, 0]} />
-          <mesh>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={color} />
-          </mesh>
-        </Canvas>
-      </div>
-
-      <div style={{ display: "flex", marginTop: "20px", gap: "20px", alignItems: "flex-start" }}>
-        <SketchPicker color={color} disableAlpha onChange={handleColorChange} />
-        <div style={{ display: "flex", flexDirection: "column", gap: "10px", maxWidth: "50px" }}>
-          {swatches.map((swatchColor, index) => (
-            <div key={index} style={{ textAlign: "center" }}>
-              <div
-                onClick={() => {
-                  if (selectedSwatchIndex === index) {
-                    setSelectedSwatchIndex(null)
-                  } else {
-                    setSelectedSwatchIndex(index)
-                    setColor(swatchColor)
-                  }
-                }}
-                style={{
-                  width: "40px",
-                  height: "40px",
-                  borderRadius: "8px",
-                  backgroundColor: swatchColor,
-                  border: selectedSwatchIndex === index ? "3px solid pink" : "1px solid pink",
-                  cursor: "pointer"
-                }}
-              />
+                <div id="colorPart" style={{ display: "flex", marginTop: "20px", gap: "20px", alignItems: "flex-start" }}>
+                    <SketchPicker color={color} disableAlpha onChange={handleColorChange} />
+                    <div style={{ display: "flex", flexDirection: "column", gap: "10px", maxWidth: "50px" }}>
+                        {swatches.map((swatchColor, index) => (
+                            <div key={index} style={{ textAlign: "center" }}>
+                                <div
+                                    onClick={() => {
+                                        if (selectedSwatchIndex === index) {
+                                            setSelectedSwatchIndex(null)
+                                        } else {
+                                            setSelectedSwatchIndex(index)
+                                            setColor(swatchColor)
+                                        }
+                                    }}
+                                    style={{
+                                        width: "40px",
+                                        height: "40px",
+                                        borderRadius: "8px",
+                                        backgroundColor: swatchColor,
+                                        border: selectedSwatchIndex === index ? "3px solid pink" : "1px solid pink",
+                                        cursor: "pointer"
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-          ))}
+
+            <div id="buttons" style={
+                    {
+                        display: "flex",
+                        flexDirection: "row"
+                    }
+                }>
+                    <button
+                        onClick={() => {
+                            setCreateBoxBtn(true)
+                            setShowInventory(prev => !prev)
+                        }}
+                    >
+                        블럭 사용하기
+                    </button>
+                    <label style={{ display: 'inline-flex', alignItems: 'center', cursor: 'pointer', gap: '6px', marginLeft: '10px', marginRight: '10px' }}>
+                        <input
+                            type="checkbox"
+                            checked={isGrid}
+                            onChange={() => setIsGrid(prev => !prev)}
+                        />
+                        <span>Grid 적용</span>
+                    </label>
+                    <button onClick={() => setBackgroundColor(color)}>배경색 적용하기</button>
+                </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default InventoryPanel
